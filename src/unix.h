7d0aff5ba3321d291ae4c7bb8b927bea5eec37f4
Joachim Nilsson <troglobit@vmlinux.org>
2010-07-18 03:37:23 +0200

    Remove obsolete checks, all current systems today have dirent.h

--- src/unix.h
+++ src/unix.h
@@ -29,16 +29,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-#ifdef USE_DIRENT
 #include <dirent.h>
 typedef struct dirent	DIRENTRY;
-#else
-#include <sys/dir.h>
-typedef struct direct	DIRENTRY;
-#endif
-
-#ifndef S_ISDIR
-#define S_ISDIR(m)		(((m) & S_IFMT) == S_IFDIR)
-#endif
 
 #endif  /* __EDITLINE_UNIX_H__ */
